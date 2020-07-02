97348aabcb172af5478985b4ff4005215649abc0
Derekhunterrc0001 <64759619+Derekhunterrc0001@users.noreply.github.com>
2020-07-01 18:05:33 -0600

    Create step173.py

---
+++ step173.py
@@ -0,0 +1,20 @@
+
+
+
+class robots:
+    modle = 'seven-T'
+    movement = 'articulated'
+    price = 'varies'
+print(robots)
+class fridge(robots):
+    capacity = '5cu ft'
+    num_of_shelfs = 3
+    num_of_drawers = 3
+print(fridge)
+class drone(robots):
+    num_of_propelers = 4
+    camara = '12Mp, 1.4um pixels'
+    battery = '5,000mAh'
+print(drone)
+
+
